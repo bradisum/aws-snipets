3f7bbbcabc46e218ea0b6673f20307da256a064c
brad182 <brad@komandra.com>
2019-09-03 16:54:16 -0400

    Minor changes to kick start.

--- ec2kickstart.sh
+++ ec2kickstart.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo yum -y install git
-
-git clone https://github.com/bradisum/aws-snipets.git
-
-chmod a+x aws-snipets/*.sh
-
 
 ssh awsprv <<'END'
 sudo yum update;
@@ -22,14 +16,7 @@ git clone https://github.com/bradisum/aws-snipets.git
 cd aws-snipets
 chmod a+x *.sh
 ./lsconfig.sh 10.0.1.131
-END
-
-
-ssh awsbas <<'END'
-sudo yum -y update
-sudo yum -y install git
-git clone https://github.com/bradisum/aws-snipets.git
-cd aws-snipets
-chmod a+x *.sh
 ./kbconfig.sh
 END
+
+
